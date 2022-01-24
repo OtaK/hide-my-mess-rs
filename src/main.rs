260b083daba523952e05d28f96ea3dbe5d49c56b
Mathieu Amiot <amiot.mathieu@gmail.com>
2022-01-24 03:36:18 +0100

    Fixed small bug

--- src/main.rs
+++ src/main.rs
@@ -188,16 +188,17 @@ fn main_next() -> error::HideResult<()> {
     let mut fake_camera = v4l::Device::new(fake_cam_info.index().as_index().unwrap() as usize)?;
     use v4l::video::Output as _;
     let fake_fmt = v4l::Format::new(w, h, v4l::FourCC::new(b"RGB3"));
-    let fake_params = v4l::video::output::Parameters::with_fps(fps);
+    let fake_params = v4l::video::output::Parameters::with_fps(format.frame_rate());
     // TODO: Use the following line once v4l2loopback supports RGBA
     // let format = v4l::Format::new(w, h, v4l::FourCC::new(b"AB24"));
     let fake_fmt = fake_camera.set_format(&fake_fmt)?;
-    let fame_params = fake_camera.set_params(&fake_params)?;
+    let fake_params = fake_camera.set_params(&fake_params)?;
     log::info!("Fake Camera found at index #{}", fake_cam_info.index());
     log::debug!(
         "Fake camera format: {:?}",
         std::str::from_utf8(&fake_fmt.fourcc.repr).unwrap()
     );
+    log::debug!("Fake camera params: {:?}", fake_params);
 
     let buf_size = camera.min_buffer_size(true);
     log::debug!("Buffer size: {buf_size}");
